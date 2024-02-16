b10f83d9023aa96ffa8e2298d5bcda6b1e86dc34
SergiiBrusov <127529976+SergiiBrusov@users.noreply.github.com>
2024-02-16 16:06:11 +0100

    the first

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,10 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
-
+//* Example
+const strNum: KeyValuePair< string, number> = {key: 'cdvbrtr', value: 12}
 export {};
